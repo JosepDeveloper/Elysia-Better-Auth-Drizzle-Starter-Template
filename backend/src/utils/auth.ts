00ec4a78776b1cd12f4aecb15fb18866877dfd40
Jose Manuel Oviedo <thejm1133@gmail.com>
2025-11-22 18:48:08 -0400

    Finish the integrations with Better-Auth

--- backend/src/utils/auth.ts
+++ backend/src/utils/auth.ts
@@ -1,10 +1,21 @@
 import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import { db } from "../database";
+import { table } from "../database/schema";
+import { user, session, account, verification } from "../../auth-schema";
 
 export const auth = betterAuth({
   secret: process.env.BETTER_AUTH_SECRET,
-  database: drizzleAdapter(db, { provider: "sqlite" }),
+  database: drizzleAdapter(db, {
+    provider: "sqlite",
+    schema: {
+      ...table,
+      user,
+      session,
+      account,
+      verification,
+    },
+  }),
   emailAndPassword: {
     enabled: true,
   },
